d4727c2683a10658d77a20cd68ecf0020dd4de45
shravan-kaja <shravan.kaja@gmail.com>
2021-01-20 21:08:55 +0530

    Added: storingDoublet Percentage

--- coinFlipCombination.sh
+++ coinFlipCombination.sh
@@ -6,6 +6,7 @@ twin=0
 for (( i=0 ; $i < $temp ; i++ ))
 do
 rand=$(($RANDOM%2))
+
 if [ $rand  -eq 1 ]
 then 
 	echo "Head Wins"
@@ -32,4 +33,50 @@ do
   echo "Value: ${arr[$KEY]}"
 done
 }
+function coinDoublet() {
+temp=$1
+HHwin=0
+HTwin=0
+THwin=0
+TTwin=0
+for (( i=0 ; $i < $temp ; i++ ))
+do
+rand=$(($RANDOM%2))
+rand1=$(($RANDOM%2))
+if [ $rand -eq 1 -a $rand1 -eq 1 ]
+then
+	echo "HH wins"
+	HHwin=$(( $HHwin + 1))
+elif [ $rand -eq 1 -a $rand1 -eq 0 ]
+then
+        echo "HT wins"
+        HTwin=$(( $HTwin + 1))
+elif [ $rand -eq 0 -a $rand1 -eq 1 ]
+then
+        echo "TH wins"
+        THwin=$(( $THwin + 1))
+elif [ $rand -eq 0 -a $rand1 -eq 0 ]
+then
+        echo "TT wins"
+        TTwin=$(( $TTwin + 1))
+else 
+	break
+fi
+done
+echo $HHwin
+x=$(($HHwin/$temp))
+HHpercentage=$(( $x * 100 ))
+echo $HTwin
+y=$(($HTwin/$temp))
+HTpercentage=$(( $y * 100 ))
+echo $THwin
+z=$(($THwin/$temp))
+THpercentage=$(( $ * 100 ))
+echo $TTwin
+d=$(($hwin/$temp))
+TTpercentage=$(( $d * 100 ))
+arr=( $HHpercentage $HTpercentage $THpercentage $TTpercentge )
+}
+
 coinFlip 7
+coinDoublet 8
